5b19345fbbff15ac9349b5f44b54ad1482ec3641
dubovykVlad <94374684+dubovykVlad@users.noreply.github.com>
2024-08-19 10:04:18 +0300

    Add files via upload

---
+++ hw2.1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "I love C++";
+    return 0;
+}
---
+++ hw2.3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+// Програма не працює через відсутність закрити лапок у cout і крапки з комою
+  cout << "Is there a bug here?";
+  return 0;
+}    
