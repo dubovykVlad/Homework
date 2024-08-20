55acc97d5bc9fdc726eaab3c45f16b1463eb9e9d
dubovykVlad <94374684+dubovykVlad@users.noreply.github.com>
2024-08-20 09:43:58 +0300

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,21 @@
+/*
+ВІДПОВІДІ НА ПИТАННЯ ПО ДЗ:
+1.  1.1. unsigned int
+    1.2. float
+    1.3. float
+*/
+#include <iostream>
+ 
+int main() {
+    unsigned int MyAge;
+    float BackyardArea;
+    unsigned long long StarsInGalaxy;
+    float AvaragePrecLvl;
+
+    const double PI = 3.14159;
+    float pi = PI;
+
+    return 0;
+}
+
+
